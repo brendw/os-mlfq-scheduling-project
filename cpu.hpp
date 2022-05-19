2eb85962dfbb335bd9df89833cf1d13430695398
Brenda  Wang <brendawang@Brendas-MacBook-Pro.local>
2022-05-19 14:49:23 -0700

    hpp files

---
+++ cpu.hpp
@@ -0,0 +1,29 @@
+#include "process.hpp"
+
+class CPU {
+
+int clock;
+
+public:
+    CPU(); //constructor
+    Process runTask(Process, int);
+};
+
+CPU::CPU() {
+    clock = 0; //?
+}
+
+Process runTask(Process p, int quantum) {
+
+    int remainingTime = p.getTimeRemaining();
+
+    if (remainingTime > quantum) {
+        p.decrementTimeRemaining(quantum);
+    }
+    else {
+        p.decrementTimeRemaining(remainingTime); //remaining is 0 
+    }
+    
+    return p; 
+
+}
